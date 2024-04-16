41109c0385ac4d94db52488a19e87980fb5b3070
Becker & Co <138133462+beckerandco@users.noreply.github.com>
2024-04-16 15:11:23 +0200

    Update morethancookies.js

--- morethancookies.js
+++ morethancookies.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         "calder.JPG",
          "cono_jade_largo.jpg",
         "pieza_unica_amatista.jpg",
-        "flores_2.jpg"
+        "flores_2.jpg",
         "pieza_unica_escarabajo.JPG",
         "foto_12.JPG",
         "margaritas_2.JPG",
@@ -24,21 +24,21 @@ document.addEventListener("DOMContentLoaded", function() {
         "pieza_unica_2.JPG",
         "isadora_3.JPG",
         "foto_14.JPG",
-        "flores_3.jpg"
+        "flores_3.jpg",
         "foto_4.jpg",
         "foto_5.jpg",
         "broche_margarita.jpg",
         "foto_6.jpg",
         "foto_7.jpg",
         "pendientes_conchas.JPG",
-         "flores_4.jpg"
+         "flores_4.jpg",
         "isadora_4.JPG",
         "foto_8.jpg",
         "pendientes_palmera.JPG",
         "negro.png",
         "manos.JPG",
         "foto_9.jpg",
-        "foto_10.jpg",
+        "foto_10.jpg"
     ];
     let tiempoIntervalo = 500;
 
